387becc1cbbcabe36992639af069026e9e18213c
juancarlomartins <juanctmartins117@gmail.com>
2026-03-12 16:56:23 -0300

    Adicionando todos os meus códigos de treino

---
+++ C++/HelloWorld.cpp
@@ -0,0 +1,11 @@
+//Autor: Juan Carlo Teles Martins.
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+//Esse é o primeiro arquivo criado nessa pasta, aqui vão ficar os treinos
+//e atividades relacionadas a essa linguagem de programação.
