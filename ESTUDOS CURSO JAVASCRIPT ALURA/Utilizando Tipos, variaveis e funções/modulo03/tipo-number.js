202ac2ac1563cd7c3fd7e00fa0544f01f47cd3b9
juancarlomartins <juanctmartins117@gmail.com>
2026-03-17 19:54:56 -0300

    Estudos de JavaScript módulo 3

--- ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/tipo-number.js
+++ ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/tipo-number.js
@@ -8,3 +8,5 @@ let media = total/4;
 
 console.log("Soma das notas: ", total);
 console.log("Média das notas: ", media.toFixed(2));//.toFixed para controlar as casas decimais do resultado final.
+console.log("\n");
+console.log(5 * '5'); /// Nesse caso o javaScript tenta converter de forma implícita a string em número antes de efetuar a operação.
---
+++ ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/tipo-string.js
@@ -0,0 +1,15 @@
+//Estudando o tipo de dado string.
+const estudante = "juan";
+const docente = 'Carlos';
+
+const cumprimento = "Nosso lema é 'Estudar bastante!'"; //Como usar aspas dentro de aspas.
+const citacao = `Ju diz "Nosso lema é 'estudar bastante!'"`;
+
+console.log(estudante);
+console.log(docente);
+console.log(cumprimento);
+console.log(citacao);
+
+// + -> também é usado para concatenar strings.
+
+console.log(`O estudante ${estudante}, é aluno do professor ${docente}, e a ${citacao}.`); //`` uso de crase no template string.
