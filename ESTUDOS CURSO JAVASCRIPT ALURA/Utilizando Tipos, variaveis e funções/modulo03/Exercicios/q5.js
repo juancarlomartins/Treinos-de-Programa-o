9fbf165b2eb33091f49de0d6c760edaa5255ed22
juancarlomartins <juanctmartins117@gmail.com>
2026-03-18 23:16:43 -0300

    atualizando código

--- ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/Exercicios/q5.js
+++ ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/Exercicios/q5.js
@@ -4,3 +4,9 @@ console.log('testando as mudanças na string.');
 console.log(nome.toUpperCase());
 console.log(nome.toLowerCase());
 console.log(nome.slice());
+
+console.log("Olá mundo!");
+
+const name = "Juan Martins";
+
+const idade = 23;
