b1583a74287c8ffaeee5051464a8e5c4c3567582
juancarlomartins <juanctmartins117@gmail.com>
2026-03-20 22:45:51 -0300

    Atualizando o código JavaScript

--- ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/Exercicios/q6.js
+++ ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/Exercicios/q6.js
@@ -3,3 +3,8 @@ console.log("Iniciando o programa.");
 let nome = prompt("Digite o seu nome ae:");
 
 console.log(`Olá ${nome}, Seja bem-vindo!`);
+
+let num = 10;
+let num2 = 20;
+
+console.log(num + num2);
