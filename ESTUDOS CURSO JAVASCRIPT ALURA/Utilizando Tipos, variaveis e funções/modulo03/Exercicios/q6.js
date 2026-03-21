86e16130018e0fa400f4524b9fde35dd83496c25
juancarlomartins <juanctmartins117@gmail.com>
2026-03-21 13:07:23 -0300

    atulizações de código

--- ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/Exercicios/q6.js
+++ ESTUDOS CURSO JAVASCRIPT ALURA/Utilizando Tipos, variaveis e funções/modulo03/Exercicios/q6.js
@@ -1,10 +1,18 @@
 //Vamos treinar um pouco.
 console.log("Iniciando o programa.");
-let nome = prompt("Digite o seu nome ae:");
+//let nome = prompt("Digite o seu nome ae:");
 
-console.log(`Olá ${nome}, Seja bem-vindo!`);
+//console.log(`Olá ${nome}, Seja bem-vindo!`);
 
-let num = 10;
-let num2 = 20;
+let pessoa1 = 20;
+let pessoa2 = 17;
 
-console.log(num + num2);
+if (pessoa1 >= 18){
+    console.log("Você é maior de idade.");
+}
+
+if (pessoa2 >= 18){
+    console.log("Você é maior de idade.");
+} else{
+    console.log("Você é menor de idade.");
+}
