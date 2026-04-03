ea18366174b727f3f70b21c1779f38fd5ba00ade
juancarlomartins <juanctmartins117@gmail.com>
2026-04-02 23:46:10 -0300

    teste

--- Programação de Computadores 2/condicionais/nota.java
+++ Programação de Computadores 2/condicionais/nota.java
@@ -1,5 +1,20 @@
 public class nota {
-    public static void main (String[] args){
-        System.out.println("Olá mundo");
-    }
+    public static void main(String[] args) {
+        int nota = 85;
+
+        if (nota >= 90){
+            System.out.println("Conceito A");
+        } else if (nota >= 80) {
+            System.out.println("Conceito B");
+        } else if (nota >= 70) {
+            System.out.println("Conceito C");
+        } else {
+            System.out.println("Conceito D - reprovado");
+        }
+
+        //Usando operador ternário (forma compacta)
+        String resultado = (nota >= 70) ? "Aprovado" : "Reprovado";
+        System.out.println("Resultado: " + resultado);
+///
+    }   
 }
