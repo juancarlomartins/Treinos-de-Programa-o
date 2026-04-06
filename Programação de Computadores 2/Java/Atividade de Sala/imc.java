1dcf864886b4668cb12d338e559428594438587e
juancarlomartins <juanctmartins117@gmail.com>
2026-04-06 17:37:23 -0300

    treinos de java

--- Programação de Computadores 2/Java/Atividade de Sala/imc.java
+++ Programação de Computadores 2/Java/Atividade de Sala/imc.java
@@ -6,6 +6,15 @@ public class imc {
 
         System.out.println("Seja bem-vindo a calculadora de imc\n");
         System.out.println("Digite aqui a sua altura: ");
-       
+        double altura = input.nextDouble();
+        System.out.println("Digite aqui o seu peso: ");
+        double peso = input.nextDouble();
+
+        //Calculando o imc.
+        double imc = peso * (altura * altura);
+        //exibindo o resultado do cálculo
+        System.out.println("O seu imc é: " + imc);
+
+        input.close();
     }
 }
