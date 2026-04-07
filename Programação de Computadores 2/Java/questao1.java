9329cd9bf9c47bd7848582cbecb6e82c22148a5e
juancarlomartins <juanctmartins117@gmail.com>
2026-04-06 22:26:26 -0300

    Atividade de programação

--- Programação de Computadores 2/Java/questao1.java
+++ Programação de Computadores 2/Java/questao1.java
@@ -1,7 +1,67 @@
-//Aluno: Juan Carlo Teles Martins
+//Aluno: Juan Carlo Teles Martins - 202504940038
+
+import java.util.Scanner;
 
 public class questao1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.println("Sistema de detecção de intrusos.\n");
+        
+        System.out.print("Digite o número de andares do prédio: ");
+        int numeroAndares = scanner.nextInt();
+        
+        int[] sensoresLigadosPorAndar = new int[numeroAndares];
+        
+        for (int andar = 1; andar <= numeroAndares; andar++) {
+            System.out.println("\n ANDAR " + andar + ".");
+            
+            System.out.print("Quantos sensores existem no " + andar + "º andar? ");
+            int quantidadeSensores = scanner.nextInt();
+            
+            int contadorLigados = 0;
+            boolean temIntruso = false;
+            
+            for (int sensor = 1; sensor <= quantidadeSensores; sensor++) {
+                System.out.print("Sensor " + sensor + " - Está ligado? (1 - Sim / 0 - Não): ");
+                int estado = scanner.nextInt();
+                
+                if (estado == 1) {
+                    contadorLigados++;
+                    temIntruso = true;
+                    System.out.println("Alerta! Movimento no Sensor " + sensor);
+                }
+            }
+            
+            sensoresLigadosPorAndar[andar - 1] = contadorLigados;
+            
+            System.out.println("\n→ No " + andar + "º andar: " + contadorLigados + " sensor(es) ligado(s)");
+            
+            if (temIntruso) {
+                System.out.println("Intruso detectado neste andar!");
+            } else {
+                System.out.println("Nenhum intruso neste andar.");
+            }
+        }
+        
+        System.out.println("RESULTADO FINAL");
+        
+        System.out.println("\nQuantidade de sensores ligados por andar:");
+        for (int i = 0; i < numeroAndares; i++) {
+            System.out.println("Andar " + (i + 1) + ": " + sensoresLigadosPorAndar[i] + " sensores ligados");
+        }
+        
+        int totalLigados = 0;
+        for (int i = 0; i < numeroAndares; i++) {
+            totalLigados += sensoresLigadosPorAndar[i];
+        }
+        
+        if (totalLigados > 0) {
+            System.out.println("\n\nAlerta, " + totalLigados + " sensores detectaram intruso!");
+        } else {
+            System.out.println("Seguro, Nenhum intruso detectado.");
+        }
+        
+        scanner.close();
     }
 }
---
+++ Programação de Computadores 2/Java/questao2.java
@@ -0,0 +1,59 @@
+//Aluno: Juan Carlo Teles Martins - 202504940038
+
+import java.util.Scanner;
+
+public class questao2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Calculadora ===");
+        System.out.println("Deseja iniciar uma operação? 1- Sim | 0- Não: ");
+        int opcao = scanner.nextInt();
+
+        while (opcao == 1){
+            int operacao;
+            System.out.println("=== Escolha ===");
+            System.out.println("1- Soma");
+            System.out.println("2- Subtração");
+            System.out.println("3- Multiplicação.");
+            System.out.println("4- Divisão.");
+            System.out.println("0- Sair.");
+            operacao = scanner.nextInt();
+
+            if (operacao == 1){
+                System.out.println("1- Somar\n");
+                System.out.println("Digite o valor do primeiro número: ");
+                double num = scanner.nextDouble();
+                System.out.println("Digite o valor do segundo número: ");
+                double num2 = scanner.nextDouble();
+                System.out.println("Resultado: " + (num + num2) + ".\n");
+            } else if (operacao == 2){
+                System.out.println("1- Subtração\n");
+                System.out.println("Digite o valor do primeiro número: ");
+                double num = scanner.nextDouble();
+                System.out.println("Digite o valor do segundo número: ");
+                double num2 = scanner.nextDouble();
+                System.out.println("Resultado: " + (num - num2) + ".\n");
+            } else if(operacao == 3) {
+                System.out.println("1- Multiplicação\n");
+                System.out.println("Digite o valor do primeiro número: ");
+                double num = scanner.nextDouble();
+                System.out.println("Digite o valor do segundo número: ");
+                double num2 = scanner.nextDouble();
+                System.out.println("Resultado: " + (num * num2) + ".\n");
+            } else if(operacao == 4){
+                System.out.println("1- Divisão\n");
+                System.out.println("Digite o valor do primeiro número: ");
+                double num = scanner.nextDouble();
+                System.out.println("Digite o valor do segundo número: ");
+                double num2 = scanner.nextDouble();
+                System.out.println("Resultado: " + (num / num2) + ".\n");
+            } else {
+                opcao = 0;
+            }
+        }
+
+        System.out.println("Fim do programa, volte sempre!");
+        scanner.close();
+    }
+}
