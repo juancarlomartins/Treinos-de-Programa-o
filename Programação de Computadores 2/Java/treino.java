6de82cf9abc848de2c06ffc6483a9cad8bc5ee1a
juancarlomartins <juanctmartins117@gmail.com>
2026-04-07 08:40:10 -0300

    ...

--- Programação de Computadores 2/Java/treino.java
+++ Programação de Computadores 2/Java/treino.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class treino {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("=== Seja be-vindo ao meu programa === ");
         
+
+        scanner.close();
     }
 }
