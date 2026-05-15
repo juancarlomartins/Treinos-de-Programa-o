cc0c9e3219d7626612ef4b0ae2895baca3f08278
juancarlomartins <juanctmartins117@gmail.com>
2026-05-15 09:26:20 -0300

    ..

--- Python/Condicionais/maiorMenor.py
+++ Python/Condicionais/maiorMenor.py
@@ -6,4 +6,4 @@ if (idade >= 18):
 else:
     print(f"Você ainda é menor de idade {nome}, mas futuramente poderá tirar sua carteira de motorista.")
     
-print(f"\nFim do programa. Volte sempre {nome}!")
+print(f"\nFim do programa. Volte sempre {nome}!")   
