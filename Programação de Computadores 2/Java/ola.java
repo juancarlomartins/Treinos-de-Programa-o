a225eafe9c692c3ce657610963b98fdeee4029be
juancarlomartins <juanctmartins117@gmail.com>
2026-05-16 12:03:30 -0300

    config vscode

--- Programação de Computadores 2/Java/ola.java
+++ Programação de Computadores 2/Java/ola.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
-
-
 public class ola {
+
+    private int numero;
+    private String nomeJogador;
+    private String nomeInimigo;
+    private String cpf;
+    private String numeroTelefone;
     public static void main(String[] args){
         // adicionei o scanner para ler a entrada do usuário, mas por enquanto ele não está sendo utilizado
+
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Olá seja bem-vindo ao meu programa!");
@@ -18,10 +23,20 @@ public class ola {
 
         boolean flag = true;
 
+        
+        String nome;
+        System.out.println("Digite o seu nome aqui carai.");
+        nome = scanner.nextLine();
+        
+        System.out.println("Olá " + nome + " seja bem-vindo!");
+
+
+        // implementação extra no código
         if(flag == true){
             scanner.close();
         } else{
             System.out.println("bora ver né");
         }
+
     }
 }
