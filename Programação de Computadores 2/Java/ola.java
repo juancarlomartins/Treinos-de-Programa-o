c834dceb6638dbd6bc970da643374c5786172d57
juancarlomartins <juanctmartins117@gmail.com>
2026-05-16 11:43:46 -0300

    alteraçẽos na configuração do vscode

--- Programação de Computadores 2/Java/ola.java
+++ Programação de Computadores 2/Java/ola.java
@@ -9,10 +9,19 @@ public class ola {
         System.out.println("Olá seja bem-vindo ao meu programa!");
         
 
-        int []vetor = {1, 2, 3, 4, 5};
+        int []vetor = {1, 2, 3, 4, 5}; 
 
         for(int i = 0; i < vetor.length; i++){
             System.out.println(vetor[i]);
+        }
+
+
+        boolean flag = true;
+
+        if(flag == true){
+            scanner.close();
+        } else{
+            System.out.println("bora ver né");
         }
     }
 }
