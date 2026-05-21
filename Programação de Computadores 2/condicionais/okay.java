fce676ab3c5a4e6a62fece6344ce21cd78cd516f
juancarlomartins <juanctmartins117@gmail.com>
2026-05-21 08:37:54 -0300

    ...

--- Programação de Computadores 2/condicionais/okay.java
+++ Programação de Computadores 2/condicionais/okay.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
 
 public class okay {
+
+    public void printDados(String nome, int idade, String curso){
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Curso: " + curso);
+    }
     public static void main(String[] args) {
         System.out.print("Olá seja bem-vindo ao meu programa brabo\n");
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
         String nome = scanner.nextLine();
 
+        int idade;
+        String curso;
 
+        System.out.print("Certo " + nome + ", agora vamos coletar alguns dados importantes.\n");
+        System.out.print("Digite a sua idade: ");
+        idade = scanner.nextInt();
+        System.out.print("Digite o curso que você faz: ");
+        curso = scanner.nextLine();
 
-        
+        System.out.println("=== Dados Coletados do Usuário ===");
+
+        int num = 1;
+
+        if (num == 1){
+            scanner.close();
+        }
     }
 }
