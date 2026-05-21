3ac891c248de3ddc534a3f9fca4d15b3cbc02673
juancarlomartins <juanctmartins117@gmail.com>
2026-05-20 23:00:00 -0300

    ...

---
+++ Programação de Computadores 2/condicionais/okay.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class okay {
+    public static void main(String[] args) {
+        System.out.print("Olá seja bem-vindo ao meu programa brabo\n");
+        Scanner scanner = new Scanner(System.in);
+        String nome = scanner.nextLine();
+
+
+
+        
+    }
+}
