692e5c4f6a263a5dee6b1670f162c44c1156de38
juancarlomartins <juanctmartins117@gmail.com>
2026-05-25 08:16:52 -0300

    ...

--- POO/Main.java
+++ POO/Main.java
@@ -7,6 +7,9 @@ public class Main {
         System.out.println("Olá seja bem-vindo ao minha loja!");
 
 
+
+
+        // para fechar o scanner...
         boolean flagDesligar = true;
         if (flagDesligar == true){
             scanner.close();
