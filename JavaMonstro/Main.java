925134dedaa5d925c116cbc387737b39548f114c
juancarlomartins <juanctmartins117@gmail.com>
2026-05-30 13:05:52 -0300

    ...

--- JavaMonstro/Main.java
+++ JavaMonstro/Main.java
@@ -3,9 +3,6 @@ public class Main{
         System.out.println("Olá seja bem-vindo,\nvamos criar um novo usuário:");
 
         // O construtor Pessoa é chamado aqui, automaticamente, pelo 'new'
-        Pessoa p1 = new Pessoa("João", 23);
-        //Agora p1.nome = "João", e p1.idade = 30;
-
-        System.out.println("Seja bem-vindo "+p1.nome+", você tem "+p1.idade+" anos.\n");
+        
     }
 }
