447152e638ec26a88ae6076621626ee9341040dc
juancarlomartins <juanctmartins117@gmail.com>
2026-05-30 13:52:02 -0300

    treino de java

--- JavaMonstro/qualquer.java
+++ JavaMonstro/qualquer.java
@@ -6,5 +6,14 @@ public class qualquer {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nOlá seja bem-vindo ao meu programa brabo\n");
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        boolean flag = true;
+        while (flag == true){
+            System.out.println("Certo " + nome + ", qual a função que você deseja usar?");
+            System.out.println("1- Calculadora Básica\n2- Calcular IMC\n3- Sair.");
+            int escolha = scanner.nextInt();
+        }
     }
 }
