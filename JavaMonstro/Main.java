aaecedbf9af9e6b6a708f6b4a4ca2c89952a90bb
juancarlomartins <juanctmartins117@gmail.com>
2026-05-30 13:40:38 -0300

    código java e organização

--- JavaMonstro/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("Olá seja bem-vindo,\nvamos criar um novo usuário:");
-
-        // O construtor Pessoa é chamado aqui, automaticamente, pelo 'new'
-        
-    }
-}
---
+++ JavaMonstro/qualquer.java
@@ -0,0 +1,10 @@
+// Juan Carlo Teles Martins
+import java.util.Scanner;
+
+public class qualquer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nOlá seja bem-vindo ao meu programa brabo\n");
+    }
+}
