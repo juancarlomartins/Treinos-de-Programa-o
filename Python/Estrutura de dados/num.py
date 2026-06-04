35a02ad7a6825ffcf87a365e9550d7a76226c210
juancarlomartins <juanctmartins117@gmail.com>
2026-06-04 10:39:07 -0300

    ...

--- Python/Estrutura de dados/num.py
+++ Python/Estrutura de dados/num.py
@@ -7,3 +7,9 @@ resultado = num + num2
 
 if (resultado == 40):
     print("Chegou até aqui essa parada...")
+
+
+contador = 1
+while(contador <= 10):
+    print(contador)
+    contador = contador + 1
