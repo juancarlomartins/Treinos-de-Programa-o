b71a8e524d3b04c1547ac17e8e4cbc49282ede85
juancarlomartins <juanctmartins117@gmail.com>
2026-06-04 23:22:31 -0300

    ...

--- EstruturaDeDados/ListaEncadeada/ListaSimplesLigada.java
+++ EstruturaDeDados/ListaEncadeada/ListaSimplesLigada.java
@@ -1,5 +1,7 @@
 package EstruturaDeDados.ListaEncadeada;
 
 public class ListaSimplesLigada {
-    
+    public static void main(String[] args) {
+        System.out.println("bora ver mano.");
+    }
 }
