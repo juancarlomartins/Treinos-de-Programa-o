e5da553c231585cb4033a4d518ef36c9b740cdae
juancarlomartins <juanctmartins117@gmail.com>
2026-06-17 18:31:39 -0300

    ..

--- Python/Condicionais/maiorMenor.py
+++ Python/Condicionais/maiorMenor.py
@@ -1,16 +1,3 @@
-nome = input("Qual o seu nome? ")
-idade = int(input(f"Seja bem-vindo {nome}, qual a sua idade? "))
-
-if (idade >= 18):
-    print(f"Parabéns {nome}! Você já pode tirar carteira de motorista.\n")
-else:
-    print(f"Você ainda é menor de idade {nome}, mas futuramente poderá tirar sua carteira de motorista.")
-    
-print(f"\nFim do programa. Volte sempre {nome}!")   
-
-
-print("Continuando o programa brabo.")
-
 n = 10
 for i in range(1, n):
     print(i)
