cf4b26742eb0ab9cb94f08e43905a18e874a54ca
Juan Carlo Teles Martins <juancarlomartins.dev@gmail.com>
2026-09-02 22:05:32 -0300

    ...

---
+++ Semestre 2026.4/Logica/C/EntradaDeDados.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int mains(){
+
+    // agora vamos trabalhar com a entrada de dados.
+    // declarando as variáveis que vamos usar.
+    int idade;
+    float altura;
+    double peso;
+
+    // vamos começar a coletar essas dados do usuário.
+    
+}
