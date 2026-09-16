cddd69e06be15ef2097d36d82aac9a37af437dd6
Juan Carlo Teles Martins <juancarlomartins.dev@gmail.com>
2026-09-16 20:45:04 -0300

    treino de logica em c

---
+++ Semestre 2026.4/Logica/listas/ExerciciosBasicos/fatorial.c
@@ -0,0 +1,26 @@
+// Criar um algoritmo que calcule o fatorial de um numero;
+
+#include <stdio.h>
+
+int main() {
+    // definindo as variáveis que vamos usar...
+    int num;
+
+    printf("Seja bem-vindo meu parceiro\n");
+    printf("Vamos calcular o fatorial, digite um número: ");
+    scanf("%d", &num);
+
+    // variáveis de controle
+    int contador = num;
+    int multiplicador = num;
+    int resultado = 0;
+    int a;
+
+    for(int i = 1; i < contador; i++){
+        multiplicador = multiplicador - 1;
+        printf("%d x %d = %d\n", num, multiplicador, (num * multiplicador));
+        
+    }
+
+    printf("\nResultado final: %d\n", resultado);
+}
